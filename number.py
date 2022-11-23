9329d5d105939f751df8845320f671404a8d4a9e
NH hackers <74050570+NH-hackers@users.noreply.github.com>
2022-11-23 20:54:48 +0530

    Update number.py

--- number.py
+++ number.py
@@ -1,6 +1,6 @@
 from bomber import selectnode
 number = ['8825322511','7836876098']
-#number = ['8935980850']
+number = ['9525461668']
 for i in number:
     selectnode(mode='sms', num=i)
     selectnode(mode='call', num=i)
