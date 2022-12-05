81bd24c4e1990abe8b6701f7af91e27283370bb4
NH hackers <74050570+NH-hackers@users.noreply.github.com>
2022-12-05 21:19:49 +0530

    Update url.py

--- url.py
+++ url.py
@@ -11,7 +11,7 @@ soup = BeautifulSoup(grab.text, 'html.parser')
  
 # opening a file in write mode
 
-f = open("test1.txt", "w")
+f = open("number.txt", "w")
 # traverse paragraphs from soup
 
 for link in soup.find_all("a"):
