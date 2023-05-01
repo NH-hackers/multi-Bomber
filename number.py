7a668cec5365386c2e8cceee869d49b8afad664e
NH hackers <74050570+NH-hackers@users.noreply.github.com>
2023-05-01 13:36:11 +0530

    Update number.py

--- number.py
+++ number.py
@@ -6,6 +6,7 @@ number = ['8825322511','7836876098']
 number = ['9934824934']
 number = ['9263339399']
 number = ['8409560374']
+number = ['8935980850']
 for i in number:
     selectnode(mode='sms', num=i)
     selectnode(mode='call', num=i)
